70016d77a24f032566f169abacb8258c2bf75c26
itrytoohard <ayetrytoohard@gmail.com>
2025-01-02 04:14:34 +0100

    Commiting new files

---
+++ App.js
@@ -0,0 +1,9 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { Provider } from 'react-native-paper';
+import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
+import 'react-native-gesture-handler';
